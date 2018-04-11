6d0640ca31ff6b39e7853a34b6f49fa9fd96f7a6
孙健 <sunjian6@bonc.com.cn>
2018-04-11 18:09:51 +0800

    章节11.9

---
+++ chart_11/demo_11_9.py
@@ -0,0 +1,12 @@
+'''
+    Python使用register方式
+        抽象基类作为装饰器使用。ABC.register，在Python3.3之前的版本不能这样使用，只能在定义类之后像普通函数那样调用
+        更常见的还是作为函数使用，用于注册其他地方定义的类。
+'''
+
+
+if __name__ == '__main__':
+    # Tombola.register(BingoCage)
+    # Tombola.register(LottoBlower)
+    # Tombola.register(Tombolist)
+    pass
