8a7d6221fd4be4dfc1f5c98fda4767cf97566da9
孙健 <sunjian6@bonc.com.cn>
2018-04-12 16:45:55 +0800

    章节14.3

---
+++ chart_14/demo_14_3.py
@@ -0,0 +1,44 @@
+'''
+    Sentence第二版:典型的迭代器
+
+'''
+
+
+import re
+import reprlib
+
+
+RE_WORD = re.compile('\w+')
+
+
+class Sentence:
+
+    def __init__(self, text):
+        self.text = text
+        self.words = RE_WORD.findall(text)
+
+    def __repr__(self):
+        return '%s(%s)' % (type(self).__name__, repr(self.text))
+
+    def __iter__(self):
+        return SentenceIterator(self.words)
+
+
+class SentenceIterator:
+
+    def __int__(self, words):
+        self.words = words
+        self.index = 0
+
+    def __next__(self):
+        try:
+            word = self.words[self.index]
+        except IndexError:
+            raise StopIteration()
+        self.index += 1
+        return word
+
+    def __iter__(self):
+        return self
+
+
